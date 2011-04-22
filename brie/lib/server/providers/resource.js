573900b54afe165d834476e09c0aa4323a6abda8
Trevor Parscal <trevorparscal@gmail.com>
2011-04-21 22:29:52 -0700

    Added special handling for .less files

--- brie/lib/server/providers/resource.js
+++ brie/lib/server/providers/resource.js
@@ -1,7 +1,9 @@
 var util = require( 'util' ),
 	events = require( 'events' ),
+	path = require( 'path' ),
 	fs = require( 'fs' ),
-	static = require('node-static');
+	static = require( 'node-static' ),
+	less = require( 'less' );
 
 var mimetypes = {
 	'.js': 'text/javascript',
@@ -15,6 +17,8 @@ var mimetypes = {
 	'.gif': 'image/gif',
 	'.jpg': 'image/jpeg'
 };
+
+var basePath = './lib/client/';
 
 function ResourceProvider( service ) {
 	events.EventEmitter.call( this );
@@ -33,7 +37,35 @@ function ResourceProvider( service ) {
 			return;
 		}
 		var ext = require( 'path' ).extname( target );
-		( new ( require( 'node-static' ).Server )( './lib/client/', {
+		// Special hacking for less files
+		if ( ext === '.less' ) {
+			path.exists( basePath + target, function( exists ) {
+				if ( exists ) {
+					fs.readFile( basePath + target, 'utf8', function( err, data ) {
+						if ( err ) {
+							throw err;
+						}
+						var parser = new ( less.Parser ) ( {
+							'paths': [path.dirname( basePath + target )],
+							'filename': path.basename( target )
+						} );
+						parser.parse( data, function ( err, tree ) {
+							if ( err ) {
+								throw err;
+							}
+							res.writeHead( 200, { 'Content-Type': 'text/css' } );
+							res.end( tree.toCSS( /* { compress: true } */ ) );
+						} );
+					} );
+				} else {
+					res.writeHead( 500, { 'Content-Type': 'text/plain' } );
+					res.end( 'Missing whirlygig.\n' );
+				}
+			} );
+			return;
+		}
+		// Normal static file delivery
+		( new ( require( 'node-static' ).Server )( basePath, {
 			'headers': {
 				'Content-Type': ext in mimetypes ? mimetypes[ext] : 'text/plain'
 			}
